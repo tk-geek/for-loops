16159c04615a63cd16e4f08939a31e627304e92d
Tanvi Kachakayala <84879323+tk-geek@users.noreply.github.com>
2021-08-04 23:16:07 +0000

    added other loops

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-for i in range (10): 
-  print("i")
+#for i in range (10): 
+#  print(i)
+
+
+#increase numbers
+for i in range ( 1 ,100, 50 ) :
+  print(i)
+
+
+#decrease numbers
+for i in range ( 10, 1, -1 ) :
+  print(i)
+
+
+#decrease numbers
+for i in range ( 100, 50, -2 ) :
+  print(i)
